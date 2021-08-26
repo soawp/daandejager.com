cbc678bedc0509e6d1d515c379afc5f3d5d65267
DaanDeJager <daandejager2002@gmail.com>
2021-08-26 16:56:19 +0200

    upgrade

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      // Build your palette here
+      primary: '#242424',
+      transparent: 'transparent',
+      current: 'currentColor',
+      gray: colors.trueGray,
+      red: colors.red,
+      blue: colors.sky,
+      yellow: colors.amber,
+    },
+    extend: {
+      
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      textColor: ['responsive', 'hover', 'focus', 'group-hover'],
+      }
   },
   plugins: [],
 }
