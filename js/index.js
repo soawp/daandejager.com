1994b6389dd9648146b4325f06266621de3bd91b
Daan de Jager <daandejager2002@gmail.com>
2022-07-08 22:12:43 +0200

    efsa

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ var upstairs = document.getElementById("upstairs");
 var computer = document.getElementById("computer");
 var char = document.getElementById("character");
 var dialog = document.getElementById("dialog");
+var loader = document.getElementById("preloader");
 
 var data = false;
 var text = '';
@@ -12,8 +13,8 @@ var typingSpeed = 50;
 var selectedRoom = 1;
 var semafoor = false;
 //start in the middle of the map
-var x = 90;
-var y = 78;
+var x = 87;
+var y = 44;
 var held_directions = []; //State of which arrow keys we are holding down
 var speed = 1; //How fast the character moves in pixels per frame
 
@@ -37,133 +38,32 @@ const placeCharacter = () => {
    } 
    if (selectedRoom === 1) {
    semafoor = false;
-   upstairs.style.backgroundImage = `url("img/upstairs.gif")`;
-   var leftLimit = -8;
-   var rightLimit = (16 * 11)+8;
+   upstairs.style.backgroundImage = `url("img/house.gif")`;
+   var leftLimit = -2;
+   var rightLimit = (16 * 11)+2;
    var topLimit = 29;
    var bottomLimit = 150;
-    // desk
-    if (x === 23 && y < 36) { x = 24; } 
-    if (x < 23 && y === 36) {selectedRoom = 4;}
-    // table 
-    if (x === 136 && y < 43) { x = 135; }
-    if (x > 144 && y < 43) { y = 43; }
-    // bed
-    if (x === 157 && y < 58) {x = 156; }
-    if (x > 156 && y < 58) { y = 58; }
-    // couch & plant
-    if (x < 8 && y === 61) { y = 60; }
-    if (x === 8 && y > 61 && y < 118) { x = 9; }
-    if (x < 6 && y === 119) {y = 120; }
-    // table
-    if (x > 14 && x < 46 && y === 64) { y = 63; }
-    if (x > 14 && x < 46 && y === 100) { y = 101; }
-    if (x === 51 && y > 64 && y < 101) { x = 52; }
-    if (x === 14 && y > 64 && y < 101) { x = 13; }
-      // couch & plant
-    if (x > 172 && y === 138) { y = 139; }
-    if (x === 170 && y > 84 && y < 140) { x = 169; }
-    if (x > 172 && y === 84) { y = 83; }
-    // stairs
-    if (x === 92 && y > 130) { selectedRoom = 2; x = 18; y = 40; }
-    if (x > 92 && x < 130 && y > 125) { y = 125; }
-    if (x === 131 && y > 130) { x = 132; }
-   } else if (selectedRoom === 2) {
-      var leftLimit = -8;
-      var rightLimit = (16 * 11)+8;
-      var topLimit = 29;
-      var bottomLimit = 150;
-      upstairs.style.backgroundImage = `url("img/downstairs.gif")`;
-      //stairs
-      if (x === 16 && y < 50) { x = 17; selectedRoom = 1; x = 90; y = 140;}
-      if (x < 16 && y === 51) { y = 52; }
-      if (x < 3 && y < 58) { x = 3; }
-      if (x < 3 && y === 58) { y = 59; }
-      //shelf
-      if (y < 33 && x === 30 ) {x = 31;}
-      if (x > 30 && x < 65 && y === 35) {y = 36;}
-      if (y < 33 && x === 67 ) {x = 68;}
-      //kitchen
-      if (y < 48 && x === 137 ) {x = 136;}
-      if (x > 137 && y === 40) {y = 41;}
-      //dinner table
-      if (x > 52 && x < 120 && y === 73 ) { y = 72; }
-      if (y > 73 && y < 107 && x === 52) { x = 51; }
-      if (x > 52 && x < 120 && y === 107 ) { y = 108; }
-      if (y > 73 && y < 107 && x === 123) { x = 124; }
-      // plant
-      if (x === 171 && y > 121) { x = 170}
-      if (x > 171 && y === 121) { y = 120;}
-      //door 
-      if (x > 78 && x < 98 && y === 29) { selectedRoom = 3; x = 88; y = 130;}
-   } else if (selectedRoom === 3) {
-      var leftLimit = -8;
-      var rightLimit = (16 * 11)+8;
-      var topLimit = 50;
-      var bottomLimit = 133;
-      //bathroom
-      upstairs.style.backgroundImage = `url("img/bathroom.gif")`;
-      // door
-      if (x > 80 && x < 97 && y === 133 ) {selectedRoom = 2; y = 30; x = 77;}
-      // middle wall
-      if (x > 36 && x < 65 && y === 78) { y = 79; }
-      if ( x === 65 && y< 78) { x = 66; }
-      if ( x === 36 && y< 78) { x = 35; }
-      // bathroom
-      if (x === -5 && y < 67) { x = -6; }
-      if (x > -5 && x < 26 && y === 67) { y = 68; showData(3);}
-      if (y > 69) { hideData();}
-      if (x === 26 && y < 67) { x = 27; }
-      // right shelves
-      if (x > 140 && y === 60) {y = 61;}
-      if (y < 60 && x === 140) { x = 139; }
-      // toilet 
-      if (y < 58 && x === 129) { x = 130; }
-      if (y < 58 && x === 107) { x = 106; }
-      if (x > 107 && x < 129 && y === 58)  { y = 59;  showData(2);}
-      if (x < 107 && x > 129 && y > 58) { hideData();}
-      // wasbak
-      if (x > 70 && x < 105 &&  y === 56) { y = 57;}
-      if (x === 70 && y < 56) { x = 69;}
-      if (x === 105 && y < 56) { x = 106;}
-   } else if (selectedRoom === 4) {
-      if (!semafoor) {
-         char.style.display = "none";
-         y = 40;
-         upstairs.style.backgroundImage = `url("img/screen.png")`;
-         computer.style.display="block"
-         semafoor = true;
-      }
-      var leftLimit = -8;
-      var rightLimit = (16 * 11)+8;
-      var topLimit = 29;
-      var bottomLimit = 150;
-       // desk
-       if (x === 23 && y < 36) { x = 24; } 
-       if (x < 23 && y === 36) {y = 37;}
-       // table 
-       if (x === 136 && y < 43) { x = 135; }
-       if (x > 144 && y < 43) { y = 43; }
-       // bed
-       if (x === 157 && y < 58) {x = 156; }
-       if (x > 156 && y < 58) { y = 58; }
-       // couch & plant
-       if (x < 8 && y === 61) { y = 60; }
-       if (x === 8 && y > 61 && y < 118) { x = 9; }
-       if (x < 6 && y === 119) {y = 120; }
-       // table
-       if (x > 14 && x < 46 && y === 64) { y = 63; }
-       if (x > 14 && x < 46 && y === 100) { y = 101; }
-       if (x === 51 && y > 64 && y < 101) { x = 52; }
-       if (x === 14 && y > 64 && y < 101) { x = 13; }
-         // couch & plant
-       if (x > 172 && y === 138) { y = 139; }
-       if (x === 170 && y > 84 && y < 140) { x = 169; }
-       if (x > 172 && y === 84) { y = 83; }
-       // stairs
-       if (x === 92 && y > 130) { selectedRoom = 2; x = 18; y = 40; }
-       if (x > 92 && x < 130 && y > 125) { y = 125; }
-       if (x === 131 && y > 130) { x = 132; }
+   //shelf
+   if (x > 170 && y === 48) { y= 47;} 
+   if (x === 170 && y > 48 && y< 135) { x = 169;} 
+   if (x > 170 && y === 135) { y= 136;} 
+   //desk
+   if (x > 28 && x < 150 && y === 58) { y= 57;} 
+   if (x === 28 && y > 58 && y< 128) { x = 27;} 
+   if (x> 28 && x < 150 && y === 128) { y= 129;} 
+   if (x === 150 && y > 58 && y< 128) { x = 151;} 
+   //bank
+   if (x < 4 && y === 44) { y= 43;} 
+   if (x === 4 && y > 44 && y< 90) { x = 5; } 
+   if (x < 4 && y === 90) { y= 91;} 
+   //interactions
+   if (x === 28 && y > 86 && y< 101) {openAboutMe()} 
+
+   
+
+
+
+
    }
    if (x < leftLimit) { x = leftLimit; }
    if (x > rightLimit) { x = rightLimit; }
@@ -315,7 +215,7 @@ function openAboutMe() {
 }
 
 function openMyWork() {
-
+   
 }
 
 function openContactMe() {
@@ -326,3 +226,9 @@ function openContactMe() {
       x.style.display = "none";
    }
 }
+
+window.addEventListener("load", function(){
+   setTimeout(function() {
+      loader.style.display = "none";
+   }, 1000)
+});
